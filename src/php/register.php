7d13313d24d054e5218a3fd3f9eb18e308ed402d
domenybence <13c-domeny@ipari.vein.hu>
2024-10-24 13:52:47 +0200

    Regisztrációs oldal létrehozása és elkezdése

---
+++ src/php/register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regisztráció</title>
+    <link rel="stylesheet" href="../css/register.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Regisztráció</h1>
+    </div>
+</body>
+</html>
