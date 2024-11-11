743de3e750eb7fba1529db2a89f3f3df4e262578
VargaMilan01 <vargamilan723@gmail.com>
2024-11-11 13:06:25 +0100

    Feliratok átírása, icon adása

--- src/web/home.php
+++ src/web/home.php
@@ -6,6 +6,7 @@
     <title>CodeOverflow</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="icon" type="image/x-icon" href="./icon.png">
 </head>
 <body class="bg-dark" style="color: white">
     <nav class="navbar navbar-expand-lg  bg-dark border-bottom border-body" data-bs-theme="dark">
@@ -27,7 +28,7 @@
               </li>
             </ul>
             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Keresés" aria-label="Search">
+              <input class="form-control me-2" type="search" placeholder="Szoftverkeresés" aria-label="Search">
               <button class="btn btn-outline-primary" type="submit">Keresés</button>
             </form>
           </div>
