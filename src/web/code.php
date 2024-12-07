d456429c0d1ae7ba9e67b864845d915d8ca60ae1
domenybence <13c-domeny@ipari.vein.hu>
2024-12-07 19:00:35 +0100

    kod oldal design folyt.

--- src/web/code.php
+++ src/web/code.php
@@ -1,7 +1,5 @@
 <?php
-
 include_once "../php_functions/db_functions.php";
-
 startSession();
 ?>
 
@@ -24,25 +22,42 @@ startSession();
             <div class="title-item-wrapper">
                 <div class="title-group">
                     <div class="title-item">Készítő</div>
-                    <div class="title-divider">-</div>
                     <div class="title-item">domebence</div>
                 </div>
+                <hr>
                 <div class="title-group">
                     <div class="title-item">Kód neve</div>
-                    <div class="title-divider">-</div>
-                    <div class="title-item">Teszt kód</div>
+                    <div class="title-item">Teszt kódnév</div>
                 </div>
+                <hr>
                 <div class="title-group">
                     <div class="title-item">Kategóriák</div>
-                    <div class="title-divider">-</div>
                     <div class="title-item">HTML, JavaScript, PHP</div>
                 </div>
-            </div>
-            <div class="title-group">
-                <div class="title-date-item">Feltöltés ideje</div>
-                <div class="title-date-divider">-</div>
-                <div class="title-date-item">2024.12.05.</div>
+                <hr>
+                <div class="title-group">
+                    <div class="title-item">Feltöltés ideje</div>
+                    <div class="title-item">2024.12.05.</div>
+                </div>
             </div>
         </div>
+        <!-- editor import -->
+        <div id="container" style="height: 200px"></div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs/loader.js"></script>
+        <script>
+            require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs' }}); 
+            require(['vs/editor/editor.main'], function () {
+                monaco.editor.setTheme('vs-dark');
+                let editor = monaco.editor.create(document.getElementById('container'), {
+                    value: `console.log("Hello world!");`,
+                    language: 'javascript'
+                });
+                function adjustEditorSize() {
+                    editor.layout();
+                }
+                window.addEventListener('resize', adjustEditorSize);
+            });
+        </script>
     </div>
 </body>
+</html>
