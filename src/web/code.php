cfb006f7b362364e51799a8d47d4724036824acc
domenybence <13c-domeny@ipari.vein.hu>
2025-01-06 11:53:56 +0100

    .

--- src/web/code.php
+++ src/web/code.php
@@ -29,7 +29,7 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content=".">
-    <title><?php echo $codename . " - " . $username ?></title>
+    <title><?php echo $username . " - " . $codename ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -49,48 +49,7 @@ else {
         <div class="main">
             <div class="title-wrapper">
                 <div class="title-item-wrapper">
-                    <div class="col">
-                        <div class="title-group upvote-wrapper">
-                            <div class="title-item svg-like-wrapper ';
-                                if($likeState === 1) {
-                                    echo "checked";
-                                }
-                                echo '
-                            ">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up-circle like-svg like-svg-empty" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up-circle-fill like-svg like-svg-full" viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
-                                </svg>
-                            </div>
-                            <div class="title-item">
-                                <p class="likes" style="user-select: none;">';
-                                    $codeLikes = getCodeLikes($codeid)[0]["likeCount"];
-                                    if($codeLikes === null) {
-                                        echo 0;
-                                    }
-                                    else {
-                                        echo $codeLikes;
-                                    }
-                                    echo '
-                                </p>
-                            </div>
-                        <div class="title-item svg-dislike-wrapper ';
-                                    if($likeState === 0) {
-                                        echo "checked";
-                                    }
-                                    echo '
-                            ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-down-circle dislike-svg-empty" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-down-circle-fill dislike-svg-full" viewBox="0 0 16 16">
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
-                            </svg>
-                        </div>
-                        </div>
-                    </div>
+                    <div class="col"></div>
                     <div class="col">
                         <div class="title-group">
                             <div class="title-item" style="user-select: none;">Feltöltő</div>
