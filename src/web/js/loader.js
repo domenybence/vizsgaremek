6f3d31304e5638669fb5657fedd35016b89a1d3a
domenybence <13c-domeny@ipari.vein.hu>
2025-01-13 10:44:17 +0100

    ...

--- src/web/js/loader.js
+++ src/web/js/loader.js
@@ -2,36 +2,53 @@ window.addEventListener("load", function (){
     translateIn();
     const links = document.querySelectorAll("a");
     for (const link of links) {
-        links.addEventListener("click", translateOut);
+        link.addEventListener("click", translateOut);
     }
 });
 
 function translateIn(){
-    const color = document.querySelectorAll(".row");
-    const color2 = document.querySelectorAll(".row2");
-    const reversedColor = Array.from(color).reverse();
-    const reversedColor2 = Array.from(color2).reverse();
-    for (let index = 0; index < reversedColor.length; index++) {
-        const row = reversedColor[index];
-        const row2 = reversedColor2[index];
-        gsap.to(row, {
-            x: "100%",
-            duration: 1.5,
-            ease: "power4.inOut",
-            delay: index * 0.03
-        });
-        gsap.to(row2, {
-            x: "100%",
-            duration: 1.5,
-            ease: "power4.inOut",
-            delay: index * 0.03
-        });
-    }
-    gsap.delayedCall(1.5 + color.length * 0.03, () => {
-        document.querySelector(".page-cover").style.display = "none";
+    gsap.to(".page-cover", {
+        duration: 0.8,
+        ease: "power4.inOut",
+        opacity: 0,
+        delay: 0.2,
+        onComplete: () => {
+            document.querySelector(".page-cover").style.display = "none";
+        }
     });
+    gsap.to(".page-cover-title", {
+        y: "-30px",
+        duration: 0.8,
+        ease: "power4.inOut",
+        opacity: 0,
+        delay: 0.2
+    })
 }
 
-function translateOut(){
-
+function translateOut(event){
+    event.preventDefault();
+    gsap.fromTo(".page-cover-title", {
+        y: "30px",
+        opacity: 0
+    },
+    {
+        y: "0",
+        duration: 0.8,
+        ease: "power4.inOut",
+        opacity: 1,
+        display: "block",
+    });
+    gsap.fromTo(".page-cover", {
+        opacity: 0
+    },
+    {
+        duration: 0.8,
+        ease: "power4.inOut",
+        opacity: 1,
+        display: "block",
+        onComplete: () => {
+            const url = event.target.href;
+            window.location.href = url;
+        }
+    });
 }
