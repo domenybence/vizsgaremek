983ab31eb841990fdf178696c5c0daddcecca754
domenybence <13c-domeny@ipari.vein.hu>
2025-01-21 10:49:16 +0100

    .

--- src/web/js/registration.js
+++ src/web/js/registration.js
@@ -326,20 +326,20 @@ async function registration() {
 function showSuccessModal() {
     if(!document.body.classList.contains("registration-wrapper")) {
         document.body.insertAdjacentHTML("beforeend", `<div class="registration-wrapper">
-            <div class="registration-popup">
-            <div class="title-container">
-            <h3>Sikeres regisztráció!</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                        <div class="registration-popup">
+                                        <div class="title-container">
+                                            <h3>Sikeres regisztráció!</h3>
+                                            <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                                             </svg>
                                         </div>
                                         <hr>
                                         <div class="content">
-                                        <p>Kellemes időtöltést és jó kódolást kívánunk!</p>
+                                            <p>Kellemes időtöltést és jó kódolást kívánunk!</p>
                                         </div>
                                         <hr>
                                         <div class="button-container">
-                                        <a id="button_login" href="./login.php">Bejelentkezés</a>
+                                            <a id="button_login" href="./login.php">Bejelentkezés</a>
                                         </div>
                                     </div>
                                     </div>`);
@@ -351,23 +351,23 @@ function showSuccessModal() {
 function showTakenUsernameModal() {
     if(!document.body.classList.contains("registration-wrapper")) {
         document.body.insertAdjacentHTML("beforeend", `<div class="registration-wrapper">
-            <div class="failed-registration-popup">
-                <div class="failed-title-container">
-                    <h3>Foglalt felhasználónév!</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                    </svg>
-                </div>
-                <hr>
-                <div class="content">
-                    <p>Kérjük válasszon másik felhasználónevet.</p>
-                </div>
-                <hr>
-                <div class="button-container">
-                    <button id="button_okay">Rendben</button>
-                </div>
-            </div>
-        </div>`);
+                                                        <div class="failed-registration-popup">
+                                                            <div class="failed-title-container">
+                                                                <h3>Foglalt felhasználónév!</h3>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                                                </svg>
+                                                            </div>
+                                                            <hr>
+                                                            <div class="content">
+                                                                <p>Kérjük válasszon másik felhasználónevet.</p>
+                                                            </div>
+                                                            <hr>
+                                                            <div class="button-container">
+                                                                <button id="button_okay">Rendben</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>`);
     }
     else {
         document.querySelector("div.registration-wrapper").style.opacity = 1;
@@ -376,23 +376,23 @@ function showTakenUsernameModal() {
 function showTakenEmailModal() {
     if(!document.body.classList.contains("registration-wrapper")) {
         document.body.insertAdjacentHTML("beforeend",`<div class="registration-wrapper">
-            <div class="failed-registration-popup">
-                <div class="failed-title-container">
-                    <h3>Foglalt email cím!</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                    </svg>
-                </div>
-                <hr>
-                <div class="content">
-                    <p>Kérjük válasszon másik emailt.</p>
-                </div>
-                <hr>
-                <div class="button-container">
-                    <button id="button_okay">Rendben</button>
-                </div>
-            </div>
-        </div>`);
+                                                    <div class="failed-registration-popup">
+                                                        <div class="failed-title-container">
+                                                            <h3>Foglalt email cím!</h3>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" id="svg_x" width="35" height="35" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                                            </svg>
+                                                        </div>
+                                                        <hr>
+                                                        <div class="content">
+                                                            <p>Kérjük válasszon másik emailt.</p>
+                                                        </div>
+                                                        <hr>
+                                                        <div class="button-container">
+                                                            <button id="button_okay">Rendben</button>
+                                                        </div>
+                                                    </div>
+                                                </div>`);
     }
     else {
         document.querySelector("div.registration-wrapper").style.opacity = 1;
