3a10e8fbb6905db0688a564988b341752bdd36d2
domenybence <13c-domeny@ipari.vein.hu>
2025-01-31 09:25:33 +0100

    cred. frontend kesz

---
+++ src/web/credsupload.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pontokvásárlás</title>
+    <link rel="stylesheet" href="/vizsgaremek/src/web/css/credsupload.css">
+    <link rel="icon" type="image/x-icon" href="./icon.png">
+    <?php include "loader.html"; ?>
+    <?php include "navbar.php"; ?>
+</head>
+<body>
+    <div class="container">
+        <div class="column">
+            <h1 class="page-title">Pontok vásárlása</h1>
+            <span>Pontok száma</span>
+            <input type="text" class="num-input">
+            <span>Ár</span>
+            <input type="text" class="num-input">
+            <button type="button" class="button-input">Vásárlás</button>
+        </div>
+    </div>
+</body>
+</html>
