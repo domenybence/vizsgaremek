9c4c6174f1f76ac0da005b5c3f22ed3825dd62c5
domenybence <13c-domeny@ipari.vein.hu>
2025-02-24 18:46:44 +0100

    ..

--- src/web/requests.php
+++ src/web/requests.php
@@ -3,7 +3,6 @@ include_once "../php_functions/php_functions.php";
 if(session_status() === PHP_SESSION_NONE) {
     startSession();
 }
-<<<<<<< HEAD
 
 $data = preparedGetdata("SELECT felkeres.id, felkeres.nev, felkeres.leiras, felkeres.statusz, felkeres.ar, felkeres.hatarido, felkeres.feltoltesi_ido, felkeres.kod_minta, felkeres.felhasznalo_id, felkeres.elvallalo_felhasznalo_id, felkeres.beadott_kod, felhasznalo.nev AS username FROM felkeres  INNER JOIN felhasznalo ON felkeres.felhasznalo_id = felhasznalo.id WHERE felkeres.id = ?;", "i", [$request]);
 
@@ -25,16 +24,6 @@ if (!$isOwner && !$isAssigned && ($_SESSION["role"] !== "admin" || $_SESSION["ro
 !$isOwner && $data["statusz"] === "nyitott" ? $canAccept = true : $canAccept = false;
 $data["elvallalo_felhasznalo_id"] == $sessionUserid ? $isSubmitted = true : $isSubmitted = false;
 
-=======
-$data = preparedGetData("SELECT felkeres.nev AS requestname, felkeres.leiras AS description, felhasznalo.nev AS username, felkeres.elvallalo_felhasznalo_id AS assignee_id, felkeres.feltoltesi_ido AS uploadtime, felkeres.ar AS offered_price FROM felkeres INNER JOIN kod ON felkeres.kod_id = kod.id INNER JOIN felhasznalo ON felhasznalo.id = felkeres.felhasznalo_id WHERE felkeres.id = ?;", "i", [$request]);
-$requestname = $data[0]["requestname"];
-$username = $data[0]["username"];
-$uploadtime = $data[0]["uploadtime"];
-$payment = $data[0]["offered_price"];
-$description = $data[0]["description"];
-$assigneeid = $data[0]["assignee_id"];
-$userid = preparedGetData("SELECT felhasznalo.id FROM felhasznalo WHERE felhasznalo.nev = ?;", "s", [$_SESSION["username"]])[0]["id"];
->>>>>>> 6ddc53096dcecddf0fe15ad01063e2dbf417fd68
 ?>
 
 <!DOCTYPE html>
@@ -46,18 +35,12 @@ $userid = preparedGetData("SELECT felhasznalo.id FROM felhasznalo WHERE felhaszn
     <title>Felkérések</title>
     <link rel="stylesheet" href="/vizsgaremek/src/web/css/requests.css">
     <link rel="stylesheet" href="/vizsgaremek/src/web/css/loader.css">
-<<<<<<< HEAD
     <link rel="stylesheet" href="/vizsgaremek/src/web/css/navbar.css">
     <link rel="icon" type="image/x-icon" href="/vizsgaremek/src/web/icon.png">
     <script src="/vizsgaremek/src/web/js/navbar.js" defer></script>
     <script src="/vizsgaremek/src/web/js/requests.js" defer></script>
     <script>
         const requestid = <?php echo $request; ?>;
-=======
-    <script>
-        const userId = <?php echo json_encode($userid); ?>;
-        const requestId = <?php echo json_encode($request); ?>;
->>>>>>> 6ddc53096dcecddf0fe15ad01063e2dbf417fd68
     </script>
 </head>
 <body>
@@ -98,7 +81,6 @@ $userid = preparedGetData("SELECT felhasznalo.id FROM felhasznalo WHERE felhaszn
                     <?php echo nl2br(htmlspecialchars($data["leiras"])); ?>
                 </div>
             </div>
-<<<<<<< HEAD
             <div class="actions">
                 <?php if($canAccept) {
                     echo    '<button class="button primary accept-request">
@@ -118,26 +100,6 @@ $userid = preparedGetData("SELECT felhasznalo.id FROM felhasznalo WHERE felhaszn
                 }
                 ?>
             </div>
-=======
-            <div class="inline-group">
-                <div class="inline-item">
-                    <p>Ár</p>
-                </div>
-                <div class="inline-item">
-                    <?php echo $payment ?>
-                </div>
-            </div>
-            <div class="inline-group">
-                <div class="inline-item">
-                    <?php echo $description ?>
-                </div>
-            </div>
-            <?php if($assigneeid === null) echo '
-                <div class="inline-group">
-                    <button class="upload-button">Felkérés elvállalása</button>
-                </div>
-            '; ?>
->>>>>>> 6ddc53096dcecddf0fe15ad01063e2dbf417fd68
         </div>
     </div>
     <script src="/vizsgaremek/src/web/js/loader.js"></script>
