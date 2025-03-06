424f6bb65aff2ffb02b6fe2f83f23b5c161bdd45
VargaMilan01 <vargamilan723@gmail.com>
2025-03-06 18:06:58 +0100

    navbar2

---
+++ src/web/navbarsearch.php
@@ -0,0 +1,50 @@
+<?php
+include_once "../php_functions/php_functions.php";
+?>
+<div class="nav-menu-button">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+    </svg>
+</div>
+<nav>
+    <div class="nav-group nav-home">
+        <a class="nav-item nav-link" href="http://localhost/vizsgaremek/codeoverflow">CodeOverflow</a>
+    </div>
+    <div class="nav-group">
+        <div class="nav-dropdown-button nav-link">
+            Kategóriák
+            <?php
+            $categories = simpleGetData("SELECT kategoria.nev FROM kategoria;")[0];
+            foreach ($categories as $category) {
+                echo '<div class="nav-dropdown-item"><a class="nav-link" href="http://localhost/vizsgaremek/kategoria/' . $category . '">' . $category . '</a></div>';
+            }
+            ?>
+        </div>
+        <a class="nav-item nav-link" href="http://localhost/vizsgaremek/src/web/browse_requests.php">Felkérések</a>
+    </div>
+    <div class="nav-group">
+        <?php
+        if($_SESSION["username"] == "Vendég") {
+            echo '<div class="nav-item nav-link"><a class="nav-item nav-link" href="http://localhost/vizsgaremek/src/web/login.php">Bejelentkezés</a></div>';
+        }
+        else {
+            echo '
+                <div class="nav-dropdown-button nav-link">'.$_SESSION["username"].'
+                    <div class="nav-dropdown-item">
+                        ';
+                        if($_SESSION["role"] == "moderator") {
+                            echo '<a class="nav-link" href="http://localhost/vizsgaremek/src/web/library.php">Könyvtár</a>
+                            <a class="nav-link" href="http://localhost/vizsgaremek/src/web/approve.php">Jóváhagyások</a>';
+                        }
+                        else if($_SESSION["role"] == "admin") {
+                            echo '<a class="nav-link" href="http://localhost/vizsgaremek/src/web/approval.php">Jóváhagyások</a>
+                            <a class="nav-link" href="http://localhost/vizsgaremek/src/web/admin.php">Admin</a>';
+                        }
+                        echo '<a class="nav-link" href="http://localhost/vizsgaremek/src/web/requests.php">Felkérések</a>
+                                <a class="nav-link" href="http://localhost/vizsgaremek/src/web/logout.php">Kijelentkezés</a>
+                    </div>
+                </div>';
+        }
+        ?>
+    </div>
+</nav>
